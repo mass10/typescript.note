4f6627dcaf164e44173f324380cced7e0eea6819
Irisawa Masaru <mass10.github@gmail.com>
2020-01-03 23:32:48 +0900

    Update main.ts

--- code/overroads/main.ts
+++ code/overroads/main.ts
@@ -2,7 +2,6 @@ module main {
 
 	/**
 	 * 不明なオブジェクトの文字列表現を得ます。
-	 * 
 	 * @param unknown オブジェクト
 	 */
 	function toString(unknown: number): string;
@@ -35,14 +34,38 @@ module main {
 	}
 
 	/**
+	 * test2 引数なし
+	 */
+	function test2(): void;
+
+	/**
+	 * test2 引数ひとつ
+	 * @param param1 
+	 */
+	function test2(param1: string): void;
+
+	/**
+	 * test2 可変長引数
+	 * @param param1 
+	 */
+	function test2(...param1: any[]): void;
+
+	/**
+	 * test2 唯一の実装
+	 * @param param1
+	 */
+	function test2(...param1: any[]): void {
+		console.log("[TRACE] <test2(...)> ", param1, param1.length);
+	}
+
+	/**
 	 * 不明なオブジェクトの文字列表現をテストします。
-	 * 
 	 * @param unknown オブジェクト
 	 */
 	function test1(unknown: any) {
 
 		const result = toString(unknown);
-		console.log("[TRACE] " + result + "");
+		console.log("[TRACE] <test1()> " + result + "");
 	}
 
 	/**
@@ -50,13 +73,24 @@ module main {
 	 */
 	export function run(): void {
 
-		test1(999.999);
-		test1("Hello! Tokyo");
-		test1({ key1: "文字列1" });
-		test1([900, 1, 29, -91]);
-		test1(main);
-		test1(test1);
-		test1(() => console.log);
+		if (true) {
+			console.log("$$$ test1 $$$");
+			test1(999.999);
+			test1("Hello! Tokyo");
+			test1({ key1: "文字列1" });
+			test1([900, 1, 29, -91]);
+			test1(main);
+			test1(test1);
+			test1(() => console.log);
+		}
+
+		if (true) {
+			console.log("$$$ test2 $$$");
+			test2();
+			test2("AAA");
+			test2(["AAA", "BBB"]);
+			test2("AAA", "BBB");
+		}
 	}
 }
 
