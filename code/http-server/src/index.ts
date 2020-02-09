09b03eaca4c4ebc0eed4988fbd750d4b76a71f77
Irisawa Masaru <mass10.github@gmail.com>
2020-02-09 19:06:00 +0900

    Update index.ts

--- code/http-server/src/index.ts
+++ code/http-server/src/index.ts
@@ -1,8 +1,15 @@
 import { MyApplication } from './MyApplication';
 
+/**
+ * エントリーポイントを定義するモジュールです。
+ */
 module Main {
 
+	/**
+	 * エントリーポイントの定義です。
+	 */
 	export function main() {
+		// アプリケーションを起動します。
 		new MyApplication().run();
 	}
 }
