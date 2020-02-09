f427cec81ce3a9a3774a77055dbae8da821cd0d9
Irisawa Masaru <mass10.github@gmail.com>
2020-02-09 19:05:55 +0900

    Update MyApplication.ts

--- code/http-server/src/MyApplication.ts
+++ code/http-server/src/MyApplication.ts
@@ -1,25 +1,28 @@
 import express, { Request, Response } from "express";
+import { root } from "./controllers";
+import { hello1 } from "./controllers/hello1";
+import { dashboard } from "./controllers/dashboard";
 
+/**
+ * アプリケーションクラスです。
+ */
 export class MyApplication {
 
+	/**
+	 * アプリケーションを起動します。
+	 */
 	public run(): void {
 
 		const app = express();
-
-		app.listen(8081, "127.0.0.1");
 
 		app.set("view engine", "ejs");
 
 		app.use("/static", express.static("public"));
 
-		app.get("/", (req: Request, res: Response) => {
-			console.log("[TRACE][GET] /");
-			res.render("index.html.ejs", {});
-		});
+		app.get("/", root.get);
+		app.get("/hello1", hello1.get);
+		app.get("/dashboard", dashboard.get);
 
-		app.get("/hello1", (req: Request, res: Response) => {
-			console.log("[TRACE][GET] /hello1");
-			res.render("hello1/index.html.ejs", {});
-		});
+		app.listen(8081, "127.0.0.1");
 	}
 }
