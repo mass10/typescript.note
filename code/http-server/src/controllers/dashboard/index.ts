f28860035f8344ae487bb2f8a7eae5b5a027289d
Irisawa Masaru <mass10.github@gmail.com>
2020-02-09 19:06:12 +0900

    Create index.ts

---
+++ code/http-server/src/controllers/dashboard/index.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+/**
+ * /dashboard/ へのリクエストを扱うモジュールです。
+ */
+export module dashboard {
+
+	/**
+	 * GET /dashboard/ のコントローラー定義です。
+	 * @param req リクエストオブジェクト
+	 * @param res レスポンスオブジェクト
+	 */
+	export const get = (req: Request, res: Response) => {
+		res.render("dashboard/index.html.ejs", {});
+	};
+}
