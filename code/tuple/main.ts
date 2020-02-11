02f5c23d772c43d3d8e8643c14cc22fcf6fc3b7d
Irisawa Masaru <mass10.github@gmail.com>
2020-02-11 23:55:53 +0900

    wip

--- code/tuple/main.ts
+++ code/tuple/main.ts
@@ -1,16 +1,41 @@
-type APIResponse = [number, string];
+namespace Main {
 
-function Call1(request: string): APIResponse {
-	return [200, request];
+	/**
+	 * API の応答を定義します。
+	 */
+	type APIResponse = [number, string];
+
+	/**
+	 * API のサンプル(1)
+	 * @param request
+	 */
+	function Call1(request: string): APIResponse {
+		return [200, request];
+	}
+
+	/**
+	 * API のサンプル(2)
+	 * @param request 
+	 */
+	function Call2(request: string): [number, string] {
+		return [404, request];
+	}
+
+	/**
+	 * アプリケーションのエントリーポイントです。
+	 */
+	export function main() {
+
+		{
+			const response = Call1("Hello");
+			console.log("[TRACE] left: %s, right: %s", response[0], response[1]);
+		}
+
+		{
+			const [status_code, status] = Call2("Hello");
+			console.log("[TRACE] left: %s, right: %s", status_code, status);
+		}
+	}
 }
 
-function Call2(request: string): [number, string] {
-	return [404, request];
-}
-
-function main() {
-	console.log(Call1("Hello"));
-	console.log(Call2("Hello"));
-}
-
-main();
+Main.main();
