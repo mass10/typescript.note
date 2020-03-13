a94db0db3401733788a2c935d3aea7029214459f
Irisawa Masaru <mass10.github@gmail.com>
2020-03-13 15:07:20 +0900

    wip

--- code/networking/proxy-by-hoxy/src/main.ts
+++ code/networking/proxy-by-hoxy/src/main.ts
@@ -1,8 +1,15 @@
+var hoxy = require('hoxy');
+var proxy = hoxy.createServer().listen(8080);
+
 function main() {
 
-	console.log("### start ###");
-	console.log("--- end ---");
+	hoxy.createServer({
+		reverse: 'http://localhost:3000',
+		tls: {
+			key: fs.readFileSync('path/to/my-server.key.pem),
+			cert: fs.readFileSync('path/to/my-server.crt.pem')
+		}
+	}).listen(443);
 }
 
 main();
-
