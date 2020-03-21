2c78b4cf974d08227a9419edc2c5964608edea59
Irisawa Masaru <mass10.github@gmail.com>
2020-03-21 14:43:30 +0900

    Update preload.ts

--- code/electron-ipc-example-1/app/src/preload.ts
+++ code/electron-ipc-example-1/app/src/preload.ts
@@ -2,35 +2,62 @@ import electron from "electron";
 
 namespace PRELOAD {
 
+	function send(channel: string, unknown: any): string {
+
+		try {
+			const message = JSON.stringify(unknown);
+			console.log("[TRACE] <exposed.send()> SEND. channel: [" + channel + "], message: [" + message + "]");
+			// 非同期(応答なし)
+			// electron.ipcRenderer.send(channel, message);
+			// 同期(応答あり)
+			const response = electron.ipcRenderer.sendSync(channel, message);
+			// newer operation ??
+			// electron.ipcRenderer.invoke(channel, message);
+			return response;
+		}
+		catch (e) {
+			console.log("[ERROR] <exposed.send()> SEND ERROR" + e);
+			return "";
+		}
+	}
+
+	function registerRecvHandler(channel: string, handler: (event: any, message: string) => {}): void {
+		if (!handler) return;
+		console.log("[TRACE] <registerRecvHandler()> ウェブアプリケーションからイベントハンドラ－が設定されました！", handler);
+		electron.ipcRenderer.on(channel, handler);
+	}
+
+	// function rendererEventhandler(event: electron.IpcRendererEvent, ...args: any[]): void {
+	// 	console.log("[TRACE] <WWW.send()> RECV: ", JSON.stringify(args));
+	// }
+
+	/**
+	 * ウェブアプリケーションにシンボルをエクスポートします。
+	 */
 	function expose(): void {
 
-		const functions = {
-			send: (channel: string, unknown: any) => {
-				try {
-					const message = JSON.stringify(unknown);
-					console.log("[TRACE] SEND!! channel: [" + channel + "], message: [" + message + "]");
-					// electron.ipcRenderer.invoke("renderer-to-main", message);
-					electron.ipcRenderer.send("renderer-to-main", message);
-				}
-				catch (e) {
-					console.log("[ERROR] SEND ERROR" + e);
-				}
-			}
+		console.log("[TRACE] <preload.expose()> called.");
+		// 公開される操作
+		const exportedOperations = {
+			send: send,
+			registerRecvHandler: registerRecvHandler
 		};
+		// 公開
 		electron.contextBridge.exposeInMainWorld(
-			"__MyApp1Object", functions
+			"__MyApp1Object", exportedOperations
 		);
 	}
 
+	/**
+	 * preload のエントリーポイントです。
+	 */
 	export function main(): void {
 
-		console.log("[TRACE] (preload) ### start ###");
-		try {
-			expose();
-		}
-		finally {
-			console.log("[TRACE] (preload) --- end ---");
-		}
+		console.log("[TRACE] <preload.main()> called.");
+		// ハンドラーの登録
+		// electron.ipcRenderer.on("main-to-renderer", rendererEventhandler);
+		// 公開操作の登録
+		expose();
 	}
 }
 
