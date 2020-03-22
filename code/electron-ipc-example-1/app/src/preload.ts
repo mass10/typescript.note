158ad3a7aa5332bb6e73ab6a8a921173a3cfdb23
Irisawa Masaru <mass10.github@gmail.com>
2020-03-22 12:02:18 +0900

    .

--- code/electron-ipc-example-1/app/src/preload.ts
+++ code/electron-ipc-example-1/app/src/preload.ts
@@ -2,10 +2,15 @@ import electron from "electron";
 
 namespace EXPOSED {
 
-	function send(channel: string, unknown: any): string {
+	/**
+	 * Main プロセスに文字列を送信します。
+	 * 
+	 * @param channel チャネルの名称
+	 * @param message 文字列
+	 */
+	export function send(channel: string, message: string): string {
 
 		try {
-			const message = JSON.stringify(unknown);
 			console.log("[TRACE] <exposed.send()> SEND. channel: [" + channel + "], message: [" + message + "]");
 			// 非同期(応答なし)
 			// electron.ipcRenderer.send(channel, message);
@@ -21,7 +26,13 @@ namespace EXPOSED {
 		}
 	}
 
-	function registerRecvHandler(channel: string, handler: (event: any, message: string) => {}): void {
+	/**
+	 * IPC メッセージハンドラーを登録します。
+	 * 
+	 * @param channel チャネルの名称
+	 * @param handler ハンドラー
+	 */
+	export function registerRecvHandler(channel: string, handler: (event: any, message: string) => {}): void {
 		if (!handler) return;
 		console.log("[TRACE] <registerRecvHandler()> ウェブアプリケーションからイベントハンドラ－が設定されました！", handler);
 		electron.ipcRenderer.on(channel, handler);
@@ -40,15 +51,24 @@ namespace PRELOAD {
 	function expose(): void {
 
 		console.log("[TRACE] <preload.expose()> called.");
+
 		// 公開される操作
-		const exportedOperations = {
-			send: send,
-			registerRecvHandler: registerRecvHandler
+		const exportedOperations: Record<string, any> = {
+			"send": EXPOSED.send,
+			"registerRecvHandler": EXPOSED.registerRecvHandler
 		};
-		// 公開
-		electron.contextBridge.exposeInMainWorld(
-			"__MyApp1Object", exportedOperations
-		);
+
+		// オブジェクトをウェブサイト側に公開します。
+		// __MyApp1Object は window オブジェクト直下の要素になります。
+		electron.contextBridge.exposeInMainWorld("__MyApp1Object", exportedOperations);
+	}
+
+	/**
+	 * ハンドラー初期化
+	 */
+	function registerIPCHandlers(): void {
+		// ハンドラーの登録
+		// electron.ipcRenderer.on("main-to-renderer", () => true);
 	}
 
 	/**
@@ -58,7 +78,7 @@ namespace PRELOAD {
 
 		console.log("[TRACE] <preload.main()> called.");
 		// ハンドラーの登録
-		// electron.ipcRenderer.on("main-to-renderer", rendererEventhandler);
+		registerIPCHandlers();
 		// 公開操作の登録
 		expose();
 	}
