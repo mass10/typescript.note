b02897567f7343ce335914f936dfbd83956705f2
Irisawa Masaru <mass10.github@gmail.com>
2020-05-15 00:25:54 +0900

    wip

--- code/regex/src/Main.ts
+++ code/regex/src/Main.ts
@@ -1,5 +1,10 @@
 namespace Main {
 
+	/**
+	 * 文字列を検査します。(郵便番号)
+	 *
+	 * @param s 文字列
+	 */
 	function isPostCode(s: String): boolean {
 		const result = s.match(/^\d{3}-?\d{4}$/);
 		if (result && 0 < result?.length) {
@@ -8,23 +13,42 @@ namespace Main {
 		return false;
 	}
 
+	/**
+	 * テキストを赤く修飾します。
+	 * @param s 
+	 */
 	function red(s: string): string {
 		return `\x1b[31m${s}\x1b[39m`;
 	}
 
+	/**
+	 * テキストを緑に修飾します。
+	 * @param s 
+	 */
 	function green(s: string): string {
 		return `\x1b[32m${s}\x1b[39m`;
 	}
 
+	/**
+	 * 評価結果の修飾
+	 *
+	 * @param b 
+	 */
 	function formatResult(b: boolean): string {
 		return b ? green("true") : red("false");
 	}
 
+	/**
+	 * 文字列を検査します。(郵便番号)
+	 */
 	function validatePostCode(s: String): void {
 		const result = isPostCode(s);
 		console.log(`[TRACE] ${JSON.stringify(s)} is postcode ? -> [${formatResult(result)}]`);
 	}
 
+	/**
+	 * エントリーポイント
+	 */
 	export function main(): void {
 		validatePostCode("");
 		validatePostCode("9");
