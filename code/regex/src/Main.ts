3f96eb7c1919545872b5edf7b55af366b6070701
Irisawa Masaru <mass10.github@gmail.com>
2020-05-15 00:23:28 +0900

    wip

--- code/regex/src/Main.ts
+++ code/regex/src/Main.ts
@@ -1,29 +1,43 @@
 namespace Main {
 
-    function isPostCode(s: String): boolean {
-        if (s.match(/^\d{3}-?\d{4}$/)) {
-            return true;
-        }
-        return false;
-    }
+	function isPostCode(s: String): boolean {
+		const result = s.match(/^\d{3}-?\d{4}$/);
+		if (result && 0 < result?.length) {
+			return true;
+		}
+		return false;
+	}
 
-    function validatePostCode(s: String): void {
-        const result = isPostCode(s);
-        console.log(`[TRACE] ${s} is postcode ? -> ${result}`);
-    }
+	function red(s: string): string {
+		return `\x1b[31m${s}\x1b[39m`;
+	}
 
-    export function main(): void {
-        validatePostCode("");
-        validatePostCode("9");
-        validatePostCode("92");
-        validatePostCode("123");
-        validatePostCode("4567");
-        validatePostCode("123-");
-        validatePostCode("-4567");
-        validatePostCode("123-4567");
-        validatePostCode("123--4567");
-        validatePostCode("090-1234-5678");
-    }
+	function green(s: string): string {
+		return `\x1b[32m${s}\x1b[39m`;
+	}
+
+	function formatResult(b: boolean): string {
+		return b ? green("true") : red("false");
+	}
+
+	function validatePostCode(s: String): void {
+		const result = isPostCode(s);
+		console.log(`[TRACE] ${JSON.stringify(s)} is postcode ? -> [${formatResult(result)}]`);
+	}
+
+	export function main(): void {
+		validatePostCode("");
+		validatePostCode("9");
+		validatePostCode("92");
+		validatePostCode("123");
+		validatePostCode("4567");
+		validatePostCode("123-");
+		validatePostCode("-4567");
+		validatePostCode("123-4567");
+		validatePostCode("123-4567\n000-0000");
+		validatePostCode("123--4567");
+		validatePostCode("090-1234-5678");
+	}
 }
 
 Main.main();
