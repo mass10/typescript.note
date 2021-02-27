84868da8e4f988651e6383f26c112c892659558d
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-02-27 21:26:15 +0900

    Update Main.ts

--- code/filter/Main.ts
+++ code/filter/Main.ts
@@ -1,14 +1,6 @@
 namespace Main {
 
-	function isValidName(name: string): boolean {
-		return 0 <= name?.indexOf("J");
-	}
-
-	function filterUsers(users: string[]): string[] {
-		return users.filter(e => isValidName(e));
-	}
-
-	export function main(): void {
+	function enumUsers(): string[] {
 		const users = [
 			"00: Jimi Hendrix",
 			"01: Aretha Franklin",
@@ -17,9 +9,50 @@ namespace Main {
 			"04: Ronnie Wood",
 			"05: Keith Richards",
 		];
+		return users;
+	}
+
+	type UserElement = { name: string, email: string, birth: string };
+
+	function reconstructUserElement(userName: string): UserElement {
+		if (userName.indexOf("i") == -1) return null;
+		return {
+			name: userName,
+			email: "",
+			birth: "",
+		};
+	}
+
+	function testConvertStructure1(): void {
+		console.log("[TRACE] $$$ 文字列を別のオブジェクトに詰め替える例 $$$");
+		const users = enumUsers();
+		const elements = users.map(reconstructUserElement).filter(e => e !== null);
+		for (const e of elements) {
+			console.log("[TRACE]", e);
+		}
+	}
+
+	function isValidName(name: string): boolean {
+		return 0 <= name?.indexOf("J");
+	}
+
+	function filterUsers(users: string[]): string[] {
+		return users.filter(isValidName);
+	}
+
+	function testFilteringUsersByName(): void {
+		const users = enumUsers();
 		for (const e of filterUsers(users)) {
 			console.log(e);
 		}
+	}
+
+	export function main(): void {
+
+		// 要素を単純に条件でフィルタリングする例
+		testFilteringUsersByName();
+		// 要素を走査して別の構造に作り変える例
+		testConvertStructure1();
 	}
 }
 
